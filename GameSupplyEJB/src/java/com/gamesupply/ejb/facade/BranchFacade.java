2c24c6de3c88af135586fb913c2a39fc0560f9c7
CUESTAS <CUESTAS@ECNP0005.gerdau.net>
2013-04-02 14:18:27 -0300

    estrutura final

--- GameSupplyEJB/src/java/com/gamesupply/ejb/facade/BranchFacade.java
+++ GameSupplyEJB/src/java/com/gamesupply/ejb/facade/BranchFacade.java
@@ -7,9 +7,13 @@ package com.gamesupply.ejb.facade;
 import com.gamesupply.dto.BranchDTO;
 import com.gamesupply.ejb.remote.BranchFacadeLocal;
 import com.gamesupply.entity.Branch;
+import java.lang.reflect.InvocationTargetException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import org.apache.commons.beanutils.BeanUtils;
 
 /**
  *
@@ -31,9 +35,16 @@ public class BranchFacade extends AbstractFacade<Branch> implements BranchFacade
 
     @Override
     public void create(BranchDTO branch) {
-       Branch b = new Branch(); 
-       b.setIdBranch(branch.getIdBranch()); 
-       super.create(b);
+        try {
+            Branch b = new Branch(); 
+            BeanUtils.copyProperties(b, branch);
+            //b.setIdBranch(branch.getIdBranch()); 
+            super.create(b);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(BranchFacade.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvocationTargetException ex) {
+            Logger.getLogger(BranchFacade.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
     
