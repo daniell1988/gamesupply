f165d4b8e3c0b02e3946e405398036f9b7e49739
Daniel <Daniel@Daniel-PC>
2013-04-08 15:55:36 -0300

    commit

--- GameSupplyEJB/src/java/com/gamesupply/teste/TesteAddressPersist.java
+++ GameSupplyEJB/src/java/com/gamesupply/teste/TesteAddressPersist.java
@@ -7,6 +7,7 @@ package com.gamesupply.teste;
 import com.gamesupply.dto.AddressDTO;
 import com.gamesupply.dto.CustomerDTO;
 import com.gamesupply.ejb.facade.AddressFacade;
+import com.gamesupply.ejb.remote.AddressFacadeRemote;
 import com.gamesupply.entity.AddressEntity;
 import java.lang.reflect.InvocationTargetException;
 import java.util.logging.Level;
@@ -23,7 +24,7 @@ import org.apache.commons.beanutils.BeanUtils;
  */
 public class TesteAddressPersist {
     @EJB
-    AddressFacade addressFacade = lookupAddressFacadeBean();
+    AddressFacadeRemote addressFacade = lookupAddressFacadeBean();
     
     public void teste(){
         
@@ -54,10 +55,10 @@ public class TesteAddressPersist {
         
     }
 
-    private AddressFacade lookupAddressFacadeBean() {
+    private AddressFacadeRemote lookupAddressFacadeBean() {
         try {
             Context c = new InitialContext();
-            return (AddressFacade) c.lookup("java:global/GameSupplyCore/AddressFacade!com.gamesupply.ejb.facade.AddressFacade");
+            return (AddressFacadeRemote) c.lookup("java:global/GameSupplyCore/AddressFacadeRemote!com.gamesupply.ejb.remote.AddressFacadeRemote");
         } catch (NamingException ne) {
             Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
             throw new RuntimeException(ne);
