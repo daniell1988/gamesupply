b0c92b9f9535235019e0db946e5e598fb32cba04
Daniel <Daniel@Daniel-PC>
2013-04-10 13:22:21 -0300

    teste concluido

--- GameSupplyEJB/src/java/com/gamesupply/teste/TesteAddressPersist.java
+++ GameSupplyEJB/src/java/com/gamesupply/teste/TesteAddressPersist.java
@@ -22,19 +22,9 @@ import javax.naming.NamingException;
  * @author Daniel
  */
 public class TesteAddressPersist {
-
-    //TestSessionBeanRemote addressFacade = lookupAddressFacadeBean();
+    
     AddressFacadeRemote addressFacade = null;
-    //TestSessionBeanLocal addressFacadeLocal = lookupAddressFacadeBeanLocal();
-    //TestSessionBeanRemote addressFacade = null;
-    
-    public void teste2() {
-        //System.out.println(addressFacadeLocal.getHelloLocal());
-        //addressFacade = lookupAddressFacadeBean();
-        //System.out.println(addressFacade.getHello());
-       // System.out.println(addressFacade.newHello());
-    }
-    
+
     public void teste(){
         addressFacade = lookupAddressFacadeBean();
         AddressDTO address = new AddressDTO();
@@ -46,15 +36,12 @@ public class TesteAddressPersist {
         address.setAddressDescription("ao lado da padaria");
         address.setCity("sao paulo");
         address.setCountry("brasil");
-   //   address.setIdAddress(Integer.MIN_VALUE);
         address.setIdCustomer(customer);
         address.setPhoneNumber("999+6999");
         address.setState("SP");
         address.setZip("22501-581");
-        
-       
+
         addressFacade.create(address);
-        
         
     }
     
@@ -66,39 +53,13 @@ public class TesteAddressPersist {
 
     private AddressFacadeRemote lookupAddressFacadeBean() {
         try {
-            //System.setProperty("java.security.auth.login.config", "C:\\Program Files\\glassfish-3.1.2.2\\glassfish\\lib\\appclient\\appclientlogin.conf");
             Properties props = new Properties();
             props.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.enterprise.naming.SerialInitContextFactory");
-            props.setProperty("org.omg.CORBA.ORBInitialHost", "10.49.31.1");
+//          props.setProperty("org.omg.CORBA.ORBInitialHost", "10.49.31.1");
+            props.setProperty("org.omg.CORBA.ORBInitialHost", "localhost");
             props.setProperty("org.omg.CORBA.ORBInitialPort", "3700");
             InitialContext ctx = new InitialContext(props);            
-            //Context c = new InitialContext();
             return (AddressFacadeRemote) ctx.lookup("java:global/GameSupplyCore/AddressFacade!com.gamesupply.ejb.remote.AddressFacadeRemote");
-            // retorna ERRO
-            //return (TestSessionBeanRemote) ctx.lookup("java:global/GameSupplyCore/TestSessionBean");
-            //return (TestSessionBeanRemote) ctx.lookup("java:global/GameSupplyCore/TestSessionBean");
-            // retorna ERRO
-            //return (TestSessionBeanRemote) ctx.lookup("com.gamesupply.ejb.test.TestSessionBean");
-            //return (TestSessionBeanRemote) ctx.lookup("java:global/GameSupplyCore/TestSessionBean!com.gamesupply.ejb.test.TestSessionBeanRemote");
-        } catch (NamingException ne) {
-            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
-            throw new RuntimeException(ne);
-        }
-    }
-
-    private TestSessionBeanLocal lookupAddressFacadeBeanLocal() {
-        try {
-            //System.setProperty("java.security.auth.login.config", "C:\\Program Files\\glassfish-3.1.2.2\\glassfish\\lib\\appclient\\appclientlogin.conf");
-            Properties props = new Properties();
-            props.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.enterprise.naming.SerialInitContextFactory");
-            props.setProperty("org.omg.CORBA.ORBInitialHost", "10.49.31.1");
-            props.setProperty("org.omg.CORBA.ORBInitialPort", "3700");
-            InitialContext ctx = new InitialContext(props);            
-            //Context c = new InitialContext();
-            return (TestSessionBeanLocal) ctx.lookup("java:global/GameSupplyCore/TestSessionBean!com.gamesupply.ejb.test.TestSessionBeanRemote");
-            //return (TestSessionBeanLocal) ctx.lookup("java:global/GameSupplyCore/TestSessionBean");
-            //return (TestSessionBeanLocal) ctx.lookup("TestSessionBean");
-            //return (TestSessionBeanLocal) ctx.lookup("com.gamesupply.ejb.test.TestSessionBean");
         } catch (NamingException ne) {
             Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
             throw new RuntimeException(ne);
