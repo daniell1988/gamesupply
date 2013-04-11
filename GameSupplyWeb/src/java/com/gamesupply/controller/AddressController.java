f4dd7a60d149cff5c6d2de508e5648808bd01ece
Daniel <Daniel@Daniel-PC>
2013-04-11 15:01:26 -0300

    teste concluido

--- GameSupplyWeb/src/java/com/gamesupply/controller/AddressController.java
+++ GameSupplyWeb/src/java/com/gamesupply/controller/AddressController.java
@@ -6,10 +6,8 @@ package com.gamesupply.controller;
 
 import com.gamesupply.dto.AddressDTO;
 import com.gamesupply.dto.CustomerDTO;
-import com.gamesupply.ejb.facade.AddressFacade;
 import com.gamesupply.ejb.remote.AddressFacadeRemote;
 import com.gamesupply.util.GSUtils;
-import javax.faces.context.FacesContext;
 
 /**
  *
@@ -24,13 +22,33 @@ public class AddressController {
     
     public void create(){
         
-        addressFacade = (AddressFacadeRemote) GSUtils.dynamicLookup(AddressFacade.class);
+        addressFacade = (AddressFacadeRemote) GSUtils.dynamicLookup(AddressFacadeRemote.class);
+        
+        customer.setIdCustomer(Integer.valueOf(1));
+        address.setAddress("Rua x");
+        address.setAddressComplement("numro 90");
+        address.setAddressDescription("ao lado da padaria");
+        address.setCity("sao paulo");
+        address.setCountry("brasil");
+        address.setIdCustomer(customer);
+        address.setPhoneNumber("999+6999");
+        address.setState("SP");
+        address.setZip("22501-581");
+        
+        addressFacade.create(address);
         
     }    
     
     public AddressController() {
         
-        FacesContext facesContext = FacesContext.getCurrentInstance();
+//        FacesContext facesContext = FacesContext.getCurrentInstance();
+        
+    }
+    
+        public static void main(String[] args){
+        
+        //new TesteAddressPersist().teste();
+        new AddressController().create();
         
     }
 }
