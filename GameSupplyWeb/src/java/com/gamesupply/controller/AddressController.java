5ed7379490fb235f1ae9885d279526e0ad09eb1b
CUESTAS <CUESTAS@ECNP0005.gerdau.net>
2013-04-11 09:58:34 -0300

    address controller

---
+++ GameSupplyWeb/src/java/com/gamesupply/controller/AddressController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gamesupply.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author CUESTAS
+ */
+@ManagedBean
+@RequestScoped
+public class AddressController {
+
+    /**
+     * Creates a new instance of AddressController
+     */
+    public AddressController() {
+    }
+}
---
+++ GameSupplyWeb/src/java/com/gamesupply/controller/CustomerController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gamesupply.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author CUESTAS
+ */
+@ManagedBean
+@SessionScoped
+public class CustomerController {
+
+    /**
+     * Creates a new instance of CustomerController
+     */
+    public CustomerController() {
+    }
+}
