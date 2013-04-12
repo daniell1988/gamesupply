c1004272459345f8672ca19718bdf7f54788f283
CUESTAS <CUESTAS@ECNP0005.gerdau.net>
2013-04-12 11:09:22 -0300

    address controller

--- GameSupplyWeb/src/java/com/gamesupply/controller/AddressController.java
+++ GameSupplyWeb/src/java/com/gamesupply/controller/AddressController.java
@@ -8,17 +8,46 @@ import com.gamesupply.dto.AddressDTO;
 import com.gamesupply.dto.CustomerDTO;
 import com.gamesupply.ejb.remote.AddressFacadeRemote;
 import com.gamesupply.util.GSUtils;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 
 /**
  *
  * @author CUESTAS
  */
+@ManagedBean
+@RequestScoped
 public class AddressController {
     
 
-    private AddressDTO address = null;
-    private CustomerDTO customer = null;
-    private AddressFacadeRemote addressFacade = null;
+    private AddressDTO address;
+    private CustomerDTO customer;
+    private AddressFacadeRemote addressFacade;
+
+    public AddressDTO getAddress() {
+        return address;
+    }
+
+    public void setAddress(AddressDTO address) {
+        this.address = address;
+    }
+
+    public CustomerDTO getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(CustomerDTO customer) {
+        this.customer = customer;
+    }
+    
+    @PostConstruct
+    public void init(){
+        
+        address = new AddressDTO();
+        customer = new CustomerDTO();
+        
+    }
     
     public void create(){
         
@@ -27,18 +56,18 @@ public class AddressController {
         } catch (Exception e) {
             System.out.println("erro lookup web");
         }
-        customer = new CustomerDTO();
-        address = new AddressDTO();
-        customer.setIdCustomer(Integer.valueOf(1));
-        address.setAddress("Rua x");
-        address.setAddressComplement("numro 90");
-        address.setAddressDescription("ao lado da padaria");
-        address.setCity("sao paulo");
-        address.setCountry("brasil");
-        address.setIdCustomer(customer);
-        address.setPhoneNumber("999+6999");
-        address.setState("SP");
-        address.setZip("22501-581");
+//        customer = new CustomerDTO();
+//        address = new AddressDTO();
+//        customer.setIdCustomer(Integer.valueOf(1));
+//        address.setAddress("Rua x");
+//        address.setAddressComplement("numro 90");
+//        address.setAddressDescription("ao lado da padaria");
+//        address.setCity("sao paulo");
+//        address.setCountry("brasil");
+//        address.setIdCustomer(customer);
+//        address.setPhoneNumber("999+6999");
+//        address.setState("SP");
+//        address.setZip("22501-581");
         
         addressFacade.create(address);
         
@@ -46,7 +75,7 @@ public class AddressController {
     
     public AddressController() {
         
-//        FacesContext facesContext = FacesContext.getCurrentInstance();
+        
         
     }
     
