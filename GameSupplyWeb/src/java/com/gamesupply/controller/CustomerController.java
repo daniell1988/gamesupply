ca0e4eb17d8d09e54229fc9b3e47a516e3082190
40836665 <40836665@093163-P31S304.macklabs.br>
2013-05-06 22:06:04 -0300

    commit address

--- GameSupplyWeb/src/java/com/gamesupply/controller/CustomerController.java
+++ GameSupplyWeb/src/java/com/gamesupply/controller/CustomerController.java
@@ -12,7 +12,6 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
 
 /**
  *
