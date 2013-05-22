fe4ec506c0a75c3947e7e926ecccd2ae1945718a
CUESTAS <CUESTAS@ECNP0005.gerdau.net>
2013-05-22 13:33:21 -0300

    stock

--- GameSupplyGUI/src/com/gamesupply/controller/ProductController.java
+++ GameSupplyGUI/src/com/gamesupply/controller/ProductController.java
@@ -9,11 +9,15 @@ import com.gamesupply.ejb.remote.ProductFacadeRemote;
 import com.gamesupply.ejb.remote.StockFacadeRemote;
 import com.gamesupply.util.GSUtils;
 
+
+
 /**
  *
  * @author CUESTAS
  */
 public class ProductController {
+    
+    private static ProductFacadeRemote productFacade;
     private static StockFacadeRemote stockFacade;
 
     public static void remove(ProductDTO productDTO) {
@@ -30,19 +34,17 @@ public class ProductController {
 
     
     
-    private ProductDTO productDTO;
-    private static ProductFacadeRemote productFacade;
+    
     
     public static void create(ProductDTO productDTO){
-        productDTO = new ProductDTO();
         try {
-//            productFacade = (ProductFacadeRemote) GSUtils.dynamicLookup("ProductFacade");
+            productFacade = (ProductFacadeRemote) GSUtils.dynamicLookup("ProductFacade");
             stockFacade = (StockFacadeRemote) GSUtils.dynamicLookup("StockFacade");
         } catch (Exception e) {
             System.out.println("erro lookup");
         }
         
-//        productFacade.create(productDTO);
+        productFacade.create(productDTO);
         stockFacade.create(productDTO);
         
         
